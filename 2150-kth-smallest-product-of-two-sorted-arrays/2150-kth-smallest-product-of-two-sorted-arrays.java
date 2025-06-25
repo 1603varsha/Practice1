e633c8ad23f70fe29ccf891d253a859110f1579b
1603varsha <139794210+1603varsha@users.noreply.github.com>
2025-06-25 09:09:36 +0530

    Time: 63 ms (99.61%), Space: 55.5 MB (50.39%) - LeetHub

--- 2150-kth-smallest-product-of-two-sorted-arrays/2150-kth-smallest-product-of-two-sorted-arrays.java
+++ 2150-kth-smallest-product-of-two-sorted-arrays/2150-kth-smallest-product-of-two-sorted-arrays.java
@@ -1,59 +1,72 @@
 class Solution {
     public long kthSmallestProduct(int[] nums1, int[] nums2, long k) {
-        List<Integer> A1 = new ArrayList<>();
-        List<Integer> A2 = new ArrayList<>();
-        List<Integer> B1 = new ArrayList<>();
-        List<Integer> B2 = new ArrayList<>();
+        int pivot1 = findFirstZero(nums1);
+        int pivot2 = findFirstZero(nums2);
+        int n1 = nums1.length, n2 = nums2.length;
+        int[] pos1 = Arrays.copyOfRange(nums1, pivot1, n1);
+        int[] pos2 = Arrays.copyOfRange(nums2, pivot2, n2);
+        int[] neg1 = negativeCopy(nums1, pivot1);
+        int[] neg2 = negativeCopy(nums2, pivot2);
 
-        separate(nums1, A1, A2);
-        separate(nums2, B1, B2);
 
-        long negCount = 1L * A1.size() * B2.size() + 1L * A2.size() * B1.size();
-        int sign = 1;
-
-        if (k > negCount) {
+        int negCount = neg1.length * pos2.length + pos1.length * neg2.length;
+        long s = 1l;
+        if(k > negCount) {
             k -= negCount;
+            s = 1l;
         } else {
-            k = negCount - k + 1;
-            sign = -1;
-            List<Integer> temp = B1;
-            B1 = B2;
-            B2 = temp;
+            int[] temp = neg2;
+            neg2 = pos2;
+            pos2 = temp;
+            s = -1l;
+            k = negCount - k +1;
         }
 
-        long l = 0, r = (long) 1e10;
-        while (l < r) {
-            long m = (l + r) / 2;
-            if (numProductNoGreaterThan(A1, B1, m) + numProductNoGreaterThan(A2, B2, m) >= k) {
-                r = m;
+        long start = 0, end = (long)Math.pow(10, 10);
+
+        while(start < end) {
+            long mid = start +(end-start)/2;
+            if((count(neg1, neg2, mid) + count(pos1, pos2, mid) )>= k) {
+                end = mid;
             } else {
-                l = m + 1;
+                start = mid+1;
             }
         }
 
-        return sign * l;
+        return start * s;
     }
 
-    private void separate(int[] nums, List<Integer> A1, List<Integer> A2) {
-        for (int a : nums) {
-            if (a < 0) {
-                A1.add(-a);
-            } else {
-                A2.add(a);
+    long count(int[] n1, int[] n2, long max) {
+        long count =0;
+        int j = n2.length-1;
+        for(int i = 0; i < n1.length; i++) {
+            while(j >= 0 && ((long)n1[i] * (long)n2[j]) > max) {
+                j-=1;
             }
+            count += j+1;
         }
-        Collections.reverse(A1);
-    }
 
-    private long numProductNoGreaterThan(List<Integer> A, List<Integer> B, long m) {
-        long count = 0;
-        int j = B.size() - 1;
-        for (int a : A) {
-            while (j >= 0 && 1L * a * B.get(j) > m) {
-                j--;
-            }
-            count += j + 1;
-        }
         return count;
+    }
+
+    private int findFirstZero(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] >= 0) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    private int[] negativeCopy(int[] nums, int len) {
+        int[] res = new int[len];
+        for (int i = 0; i < len; i++) {
+            res[i] = -1 * nums[len - 1 - i];
+        }
+        return res;
     }
 }
